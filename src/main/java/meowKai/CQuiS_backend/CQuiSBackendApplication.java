97d82986ec15dcaeb56076f5dd4378066304c29e
kklee0930 <kklee0930@gmail.com>
2024-10-19 00:28:18 +0900

    241019 initial repository setting completed

---
+++ src/main/java/meowKai/CQuiS_backend/CQuiSBackendApplication.java
@@ -0,0 +1,13 @@
+package meowKai.CQuiS_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CQuiSBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CQuiSBackendApplication.class, args);
+	}
+
+}
