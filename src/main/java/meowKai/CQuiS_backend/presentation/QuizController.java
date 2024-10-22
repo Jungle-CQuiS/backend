6c08634449bfe39cad6a742241d08eaae8924427
yujin0124 <yujin0124@khu.ac.kr>
2024-10-22 21:57:20 +0900

    refactor: rename classes and enhance code structure / 241022 / complete

--- src/main/java/meowKai/CQuiS_backend/presentation/QuizController.java
+++ src/main/java/meowKai/CQuiS_backend/presentation/QuizController.java
@@ -1,7 +1,9 @@
 package meowKai.CQuiS_backend.presentation;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
-import meowKai.CQuiS_backend.application.GradeService;
+import meowKai.CQuiS_backend.application.QuizService;
 import meowKai.CQuiS_backend.dto.request.RequestGradeDto;
 import meowKai.CQuiS_backend.dto.response.ResponseGradeDto;
 import meowKai.CQuiS_backend.global.base.ApiResponse;
@@ -11,12 +13,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/quiz/multi")
+@RequestMapping("/quiz")
 @RequiredArgsConstructor
-public class GradeController {
+public class QuizController {
 
-    private final GradeService gradeService;
+    private final QuizService gradeService;
 
+    @Tag(name = "채점")
+    @Operation(summary = "유사도 검사 바탕 문제 채점")
     @PostMapping("/grade")
     public ApiResponse<Object> checkGrade(@RequestBody RequestGradeDto requestGradeDto) {
         ResponseGradeDto responseDto = gradeService.checkGrade(requestGradeDto);
