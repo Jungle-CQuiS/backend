6fd16a98a6da7047818e8b851cddc9f6879f9ce9
kklee0930 <kklee0930@gmail.com>
2024-10-23 20:38:29 +0900

    fix: cors minor fix / 241023 / on going

--- src/main/java/meowKai/CQuiS_backend/config/webconfig/WebConfig.java
+++ src/main/java/meowKai/CQuiS_backend/config/webconfig/WebConfig.java
@@ -12,10 +12,10 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("**")
                 .allowedOrigins("http://cquis.net")
-                .allowedOrigins("https://localhost:3000")
+                .allowedOrigins("http://localhost:3000")
                 .allowedOriginPatterns("http://cquis.net/*")
-                .allowedOriginPatterns("https://localhost:*")
-                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedOriginPatterns("http://localhost:*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
     }
