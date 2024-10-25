e586b640ea3bf46027572bb19a82ca7fe28e64d5
yujin0124 <yujin0124@khu.ac.kr>
2024-10-25 20:22:19 +0900

    refactor: remove SockJS in WebSocket protocol / 241025 / complete

--- src/main/java/meowKai/CQuiS_backend/config/websocket/WebSocketConfig.java
+++ src/main/java/meowKai/CQuiS_backend/config/websocket/WebSocketConfig.java
@@ -25,5 +25,6 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
         registry.addEndpoint("/ws")
                 .setAllowedOrigins("*")
                 .withSockJS();
+
     }
 }
