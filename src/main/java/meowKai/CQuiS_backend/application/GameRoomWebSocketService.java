068e7ba056feb7c0798f5ef6b26de8ddcf01fe7e
yujin0124 <yujin0124@khu.ac.kr>
2024-10-25 16:30:38 +0900

    feat: create WebSocket service layer / 241025 / on going

---
+++ src/main/java/meowKai/CQuiS_backend/application/GameRoomWebSocketService.java
@@ -0,0 +1,14 @@
+package meowKai.CQuiS_backend.application;
+
+import meowKai.CQuiS_backend.dto.request.*;
+import meowKai.CQuiS_backend.dto.response.*;
+
+public interface GameRoomWebSocketService {
+    ResponseGetRoomInfoDto switchTeam(RequestSwitchTeamDto requestSwitchTeamDto); // 유저의 팀 바꾸기
+    ResponseGetRoomInfoDto ready(RequestReadyDto requestReadyDto); // 준비하기
+    ResponseGetRoomInfoDto kickUser(RequestKickUserDto requestKickUserDto); // 유저 강퇴(방장 권한 필요)
+    ResponseGetRoomInfoDto changeHost(RequestYieldDto requestYieldDto); // 방장 권한 위임
+    ResponseGetRoomInfoDto changeLeader(RequestYieldDto requestYieldDto); // 리더 권한 위임
+    ResponseGetRoomInfoDto exit(RequestExitDto requestExitDto); // 현재 들어와 있는 방에서 퇴장
+    ResponseGetRoomInfoDto joinRoom(RequestJoinRoomDto requestJoinRoomDto); // 방 입장
+}
