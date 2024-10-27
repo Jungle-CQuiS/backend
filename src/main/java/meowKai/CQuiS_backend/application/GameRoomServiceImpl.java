38e81e2c950b4091b115a8c27da699e6ce776755
yujin0124 <yujin0124@khu.ac.kr>
2024-10-27 15:15:40 +0900

    feat: add automatic team assignment logic / 241027 / complete

--- src/main/java/meowKai/CQuiS_backend/application/GameRoomServiceImpl.java
+++ src/main/java/meowKai/CQuiS_backend/application/GameRoomServiceImpl.java
@@ -318,6 +318,9 @@ public class GameRoomServiceImpl implements GameRoomService {
 
         // TODO: 추후에 팀 랜덤 배정 구현
         RoomUser joinedRoomUser = RoomUser.createRoomUser(foundRoom, joinUser, RoomUserRole.GUEST, RoomUserTeam.BLUE);
+        if (isTeamFull(foundRoom, RoomUserTeam.BLUE)) {
+            joinedRoomUser.changeTeam();
+        }
 
         // 방이 비어있으면 joinedRoomUser를 host, leader로 <- 이런 일이 있을 수 있나?
         if(countRoomUser(foundRoom) <= 0) {
@@ -488,5 +491,13 @@ public class GameRoomServiceImpl implements GameRoomService {
     private boolean isTeamEmpty(GameRoom gameRoom, RoomUserTeam teamColor) {
         return gameRoom.getRoomUsers().stream()
                 .noneMatch(user -> user.getTeam() == teamColor);
+    }
+
+    // 팀이 가득 찼는지 확인
+    private boolean isTeamFull(GameRoom gameRoom, RoomUserTeam teamColor) {
+        return gameRoom.getRoomUsers().stream()
+                .filter(user -> user.getTeam() == teamColor)
+                .count()
+                >= (gameRoom.getMaxUsers() / 2);
     }
 }
