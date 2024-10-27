f69b3dd0c008039c1f4292943c9f1954146a9ed2
yujin0124 <yujin0124@khu.ac.kr>
2024-10-27 21:40:58 +0900

    refactor: Seperate room creation from host user joining / 241027 / complete

--- src/main/java/meowKai/CQuiS_backend/application/GameRoomServiceImpl.java
+++ src/main/java/meowKai/CQuiS_backend/application/GameRoomServiceImpl.java
@@ -68,22 +68,12 @@ public class GameRoomServiceImpl implements GameRoomService {
                 requestDto.getPassword()
         );
 
-        RoomUser roomUser = RoomUser.createRoomUser(createdRoom,
-                userRepository.findByUuid(requestDto.getUuid()).orElseThrow(
-                        () -> new NoSuchElementException("멀티 게임 방 생성 - 존재하지 않는 유저입니다."))
-                , RoomUserRole.HOST, RoomUserTeam.BLUE);
-        roomUser.assignTeamLeader(RoomUserTeam.BLUE);
-
-        roomUserRepository.save(roomUser);
         gameRoomRepository.save(createdRoom);
 
         ResponseCreateMultiRoomDto responseDto = ResponseCreateMultiRoomDto.builder()
                 .roomId(createdRoom.getId())
-                .role(roomUser.getRole())
-                .isLeader(roomUser.getIsLeader())
-                .team(roomUser.getTeam())
                 .build();
-        log.info("멀티 게임 방 생성 - 멀티 게임 방 생성 한 유저의 정보: {}", responseDto);
+        log.info("멀티 게임 방 생성 - 멀티 게임 방 생성 결과: {}", responseDto);
         return responseDto;
     }
 
