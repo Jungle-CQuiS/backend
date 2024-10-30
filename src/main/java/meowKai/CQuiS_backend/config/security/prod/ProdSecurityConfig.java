90eb55d2e1099694d0d8329f284bc706e5d74ff5
kklee0930 <kklee0930@gmail.com>
2024-10-31 08:27:44 +0900

    fixed: cors setting / 241031 / complete

--- src/main/java/meowKai/CQuiS_backend/config/security/prod/ProdSecurityConfig.java
+++ src/main/java/meowKai/CQuiS_backend/config/security/prod/ProdSecurityConfig.java
@@ -46,10 +46,10 @@ public class ProdSecurityConfig {
                 // CORS 설정
                 .cors(cors -> cors.configurationSource(prodCorsConfig.corsConfigurationSource()))
                 // JWT 사용하기 때문에 세션 상태 STATELESS로 설정
-//                .sessionManagement(smc -> smc.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .sessionManagement(smc -> smc.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 // TODO: https 프로토콜 요청만 허용하도록 나중에 바꿔야함.
                 // https 프로토콜 요청만 허용
-                .requiresChannel(rcc -> rcc.anyRequest().requiresSecure())
+//                .requiresChannel(rcc -> rcc.anyRequest().requiresSecure())
                 // csrf 비활성화
                 .csrf(AbstractHttpConfigurer::disable)
                 .formLogin(AbstractHttpConfigurer::disable)
