44cc68c6dc31d9e760e7c9251ca89fed9c1afdb2
yujin0124 <yujin0124@khu.ac.kr>
2024-11-01 14:51:37 +0900

    feat: modify login API to return nickname / 241101 / complete

--- src/main/java/meowKai/CQuiS_backend/dto/response/ResponseLoginDto.java
+++ src/main/java/meowKai/CQuiS_backend/dto/response/ResponseLoginDto.java
@@ -12,7 +12,7 @@ import java.util.UUID;
 public class ResponseLoginDto {
 
     private UUID uuid;
+    private String username;
     private String accessToken;
     private String refreshToken;
-    private String username;
 }
