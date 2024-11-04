05061f7a10f4611223151c8720bc9c3053096589
yujin0124 <yujin0124@khu.ac.kr>
2024-11-04 17:37:12 +0900

    fix: Add enum Korean mapping and normalize uri case / 241104 / complete

--- src/main/java/meowKai/CQuiS_backend/domain/CategoryType.java
+++ src/main/java/meowKai/CQuiS_backend/domain/CategoryType.java
@@ -1,5 +1,31 @@
 package meowKai.CQuiS_backend.domain;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+import lombok.Getter;
+
+@Getter
 public enum CategoryType {
-    ALGORITHM, DATABASE, DATASTRUCTURE, NETWORK, OS
+    ALGORITHM("알고리즘"),
+    DATABASE("데이터베이스"),
+    DATASTRUCTURE("자료구조"),
+    NETWORK("네트워크"),
+    OS("OS");
+
+    @JsonValue
+    private final String koreanFieldName;
+
+    @JsonCreator
+    public static CategoryType from(String koreanFieldName) {
+        for (CategoryType type : CategoryType.values()) {
+            if(type.getKoreanFieldName().equals(koreanFieldName)) {
+                return type;
+            }
+        }
+        throw new IllegalArgumentException("Invalid category type: " + koreanFieldName);
+    }
+
+    CategoryType(String koreanFieldName) {
+        this.koreanFieldName = koreanFieldName;
+    }
 }
