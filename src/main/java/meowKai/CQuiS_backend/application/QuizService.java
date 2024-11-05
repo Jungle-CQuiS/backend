d5e2626e360f54358e183f45da1de244dab4571a
yujin0124 <yujin0124@khu.ac.kr>
2024-11-05 21:41:04 +0900

    feat: Add random quiz selection API for TimeAttack mode / 241105 / complete

--- src/main/java/meowKai/CQuiS_backend/application/QuizService.java
+++ src/main/java/meowKai/CQuiS_backend/application/QuizService.java
@@ -1,10 +1,7 @@
 package meowKai.CQuiS_backend.application;
 
 import meowKai.CQuiS_backend.domain.GameRoom;
-import meowKai.CQuiS_backend.dto.request.RequestGetChoiceAnswerQuizzesDto;
-import meowKai.CQuiS_backend.dto.request.RequestGetShortAnswerQuizzesDto;
-import meowKai.CQuiS_backend.dto.request.RequestGradeDto;
-import meowKai.CQuiS_backend.dto.request.RequestSaveSingleGameStatisticsDto;
+import meowKai.CQuiS_backend.dto.request.*;
 import meowKai.CQuiS_backend.dto.response.*;
 
 public interface QuizService {
@@ -12,6 +9,7 @@ public interface QuizService {
     ResponseGetCategoriesDto getCategories(); // 카테고리 종류 받기
     ResponseGetShortAnswerQuizzesDto getShortAnswerQuizzesByConditions(RequestGetShortAnswerQuizzesDto requestDto); // 주관식 문제 요청하기
     ResponseGetChoiceAnswerQuizzesDto getChoiceAnswerQuizzesByConditions(RequestGetChoiceAnswerQuizzesDto requestDto); // 객관식 문제 요청하기
+    ResponseGetMixAnswerQuizzesDto getMixAnswerQuizzesByConditions(RequestGetMixAnswerQuizzesDto requestDto); // 주관식 + 객관식 문제 요청하기
     ResponseGetRandomQuizzesByCategoriesDto getRandomQuizzesByCategories(Long roomId); // 카테고리 별로 랜덤 문제 두 문제씩 가져오기
     ResponseSaveSingleGameStatisticsDto saveStatisticsForSingleGame(RequestSaveSingleGameStatisticsDto requestDto); // 진행한 싱글 게임에 대한 통계 정보 저장
     void storeQuizzes(GameRoom gameRoom); // 게임 시작 직전 호출되어 카테고리별로 20개의 문제를 저장해 둠
