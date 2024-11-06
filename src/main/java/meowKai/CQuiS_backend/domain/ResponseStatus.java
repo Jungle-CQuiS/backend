0d08a4b71bb7ea56ef730900ed7f9b3733536225
yujin0124 <yujin0124@khu.ac.kr>
2024-11-06 20:44:32 +0900

    feat: change WebSocket API path and DTOs / 241106 / complete

--- src/main/java/meowKai/CQuiS_backend/domain/ResponseStatus.java
+++ src/main/java/meowKai/CQuiS_backend/domain/ResponseStatus.java
@@ -1,5 +1,5 @@
 package meowKai.CQuiS_backend.domain;
 
 public enum ResponseStatus {
-    QUIZ_SELECT, FINAL_SELECT, ANSWER_SELECT, ALL_SUBMIT
+    QUIZ_SELECT, FINAL_SELECT, ANSWER_SELECT, ALL_SUBMIT, ROUND_END, GAME_END
 }
--- src/main/java/meowKai/CQuiS_backend/dto/response/ResponseSubmitTeamDto.java
+++ src/main/java/meowKai/CQuiS_backend/dto/response/ResponseSubmitTeamDto.java
@@ -1,6 +1,8 @@
 package meowKai.CQuiS_backend.dto.response;
 
 import lombok.*;
+import meowKai.CQuiS_backend.domain.ResponseStatus;
+import meowKai.CQuiS_backend.domain.RoomUserTeam;
 
 @Builder
 @Getter
@@ -12,4 +14,6 @@ public class ResponseSubmitTeamDto {
     private Boolean isCorrect;
     private String answer;
     private Integer teamHp;
+    private ResponseStatus responseStatus;
+    private RoomUserTeam nextOffenseTeam;
 }
